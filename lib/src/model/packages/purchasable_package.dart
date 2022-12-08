dfa72b3052cf14cf0a63d3be34ff2151a4f7fdc4
yakovliam <jacob@yakovliam.com>
2022-12-08 01:19:48 -0500

    feat: add purchasble package

---
+++ lib/src/model/packages/purchasable_package.dart
@@ -0,0 +1,13 @@
+class PurchasablePackage {
+  String name;
+  String price;
+  int numberOfLoads;
+  String handle;
+
+  PurchasablePackage({
+    required this.name,
+    required this.price,
+    required this.numberOfLoads,
+    required this.handle,
+  });
+}
