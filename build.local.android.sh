582cd52f300ec106ebd00bc9b6aa5b044f344ebb
yakovliam <jacob@yakovliam.com>
2023-01-05 13:51:33 -0500

    feat: change build scripts to ios and android respectively

---
+++ build.local.android.sh
@@ -0,0 +1,5 @@
+# shellcheck disable=SC2046
+export $(grep -v '^#' .env.local | xargs)
+
+flutter pub run flutter_launcher_icons
+flutter build android --dart-define=SUPABASE_URL="$SUPABASE_URL" --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY" -t lib/main.dart
