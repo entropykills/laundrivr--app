965e721113f1bf7e1c49a6b595b75226c75e1ada
yakovliam <jacob@yakovliam.com>
2023-01-23 16:55:22 -0500

    feat: update build gradle for different abi

--- build.local.android.sh
+++ build.local.android.sh
@@ -3,3 +3,4 @@ export $(grep -v '^#' .env.local | xargs)
 
 flutter pub run flutter_launcher_icons
 flutter build apk --dart-define=SUPABASE_URL="$SUPABASE_URL" --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY" -t lib/main.dart
+flutter build appbundle
