e921539f4cdd89281c2427872c008f6efcc7d98a
yakovliam <jacob@yakovliam.com>
2023-01-26 18:43:23 -0500

    feat: add dart defines to appbundle build

--- build.local.android.sh
+++ build.local.android.sh
@@ -3,4 +3,4 @@ export $(grep -v '^#' .env.local | xargs)
 
 flutter pub run flutter_launcher_icons
 flutter build apk --dart-define=SUPABASE_URL="$SUPABASE_URL" --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY" -t lib/main.dart
-flutter build appbundle
+flutter build appbundle --dart-define=SUPABASE_URL="$SUPABASE_URL" --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY"
